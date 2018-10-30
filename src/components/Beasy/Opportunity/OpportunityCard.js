8907b090b85f407e5c833af2c44228ed857a6405
sharonKerz <sharonkerz@gmail.com>
2018-10-30 20:09:28 +0200

    adding collapse to card

--- src/components/Beasy/Opportunity/OpportunityCard.js
+++ src/components/Beasy/Opportunity/OpportunityCard.js
@@ -5,18 +5,27 @@ import {
   Card,
   CardBody,
   Button,
-  Badge
+  Badge,
+  Collapse
 } from "reactstrap";
 import Rating from "Components/Rating";
 
 class OpportunityCard extends Component {
-  state = {
-    isButtonActive: false
+  constructor(...params) {
+    super(...params);
+    this.toggleCard = this.toggleCard.bind(this);
   }
-  checkButton = () => {
-    var isButtonActive = !this.state.isButtonActive
-    this.setState({ isButtonActive: isButtonActive });
+  state = {
+    isButtonActive: false,
+    collapse: false
+  }
+  checkButton() {
+    this.setState({ isButtonActive: !this.state.isButtonActive });
   };
+
+  toggleCard() {
+    this.setState({ collapse: !this.state.collapse });
+  }
 
 
   render() {
@@ -27,11 +36,6 @@ class OpportunityCard extends Component {
     const { product } = this.props;
     return (
       <Card className="mb-4">
-        <div className="position-absolute card-top-buttons">
-          <Button outline color={"white"} className="icon-button">
-            <i className="simple-icon-pencil" />
-          </Button>
-        </div>
         <img
           src={product.img}
           alt="Detail"
@@ -51,104 +55,116 @@ class OpportunityCard extends Component {
             <b> <IntlMessages id={product.title} /> </b>
           </p>
           <p className="mb-3">
-            {product.description}
-          </p>
-
-          <p className="text-muted mb-2">
-            <IntlMessages id="Target Audiance" />
-          </p>
-          <p className="mb-4" text-large>
-             {product.gender.map(g => {
-              if (g === "Female") {
-                return <i style={iStyle} className="iconsmind-Girl" />
-              }
-              if (g === "Male") {
-                return <i style={iStyle} className="iconsmind-Boy" />
-              }
-            })}
-          </p>
-          <p className="mb-3">
-            <IntlMessages id="Age: " />
-            {product.ageMin} - {product.ageMax}
-          </p>
-
-          <p className="mb-3">
-            <i className="simple-icon-location-pin" />
-            <IntlMessages id={" " + product.location} />
-          </p>
-
-
+              {product.notes}
+            </p>
+          <Collapse isOpen={this.state.collapse}>
           <p className="text-muted text-small mb-2">
-            <IntlMessages id="Interests" />
+            <IntlMessages id={"About " + product.name} />
           </p>
-          <div className="mb-3">
-            <p className="d-sm-inline-block mb-1">
-              {product.interests.map(i => {
-                return (
-                  <Badge color="outline-secondary mb-1 mr-1" pill>
-                    {i}
-                  </Badge>
-                )
+            <p className="mb-3">
+              {product.description}
+            </p>
+
+            <p className="text-muted mb-2">
+              <IntlMessages id="Target Audiance" />
+            </p>
+            <p className="mb-4" text-large>
+              {product.gender.map(g => {
+                if (g === "Female") {
+                  return <i style={iStyle} className="iconsmind-Girl" />
+                }
+                if (g === "Male") {
+                  return <i style={iStyle} className="iconsmind-Boy" />
+                }
               })}
             </p>
-          </div>
-
-          <p className="text-muted text-small mb-2">
-            <IntlMessages id="Goals" />
-          </p>
-          <div className="mb-3">
-            <p className="d-sm-inline-block mb-1">
-              {product.goals.map(i => {
-                return (
-                  <Badge color="outline-info mb-1 mr-1" pill>
-                    {i}
-                  </Badge>
-                )
-              })}
+            <p className="mb-3">
+              <IntlMessages id="Age: " />
+              {product.ageMin} - {product.ageMax}
             </p>
-          </div>
 
-          <p className="text-muted text-small mb-2">
-            <IntlMessages id="How" />
-          </p>
-          <div className="mb-3">
-            <p className="d-sm-inline-block mb-1">
-              {product.how.map(i => {
-                return (
-                  <Badge color="outline-info mb-1 mr-1" pill>
-                    {i}
-                  </Badge>
-                )
-              })}
+            <p className="mb-3">
+              <i className="simple-icon-location-pin" />
+              <IntlMessages id={" " + product.location} />
             </p>
-          </div>
 
-          <p className="text-muted text-small mb-2">
-            <IntlMessages id="Extra Notes" />
-          </p>
-          <p className="mb-3">
-            {product.notes}
-          </p>
-          <p className="text-muted text-small mb-2">
-            <IntlMessages id="Match" />
-          </p>
-          <div className="mb-3">
-            <Rating total={5} rating={5} interactive={false} />
-          </div>
 
-          <p className="text-muted text-small mb-2">
-            <IntlMessages id="Add to Favorites" />
-          </p>
-          <Button
-            outline
-            color="secondary"
-            className="mb-2"
-            onClick={this.checkButton}
-            active={this.state.isButtonActive}
-          >
-            <i className="simple-icon-heart" />
+            <p className="text-muted text-small mb-2">
+              <IntlMessages id="Interests" />
+            </p>
+            <div className="mb-3">
+              <p className="d-sm-inline-block mb-1">
+                {product.interests.map(i => {
+                  return (
+                    <Badge color="outline-secondary mb-1 mr-1" pill>
+                      {i}
+                    </Badge>
+                  )
+                })}
+              </p>
+            </div>
 
-          </Button>
+            <p className="text-muted text-small mb-2">
+              <IntlMessages id="Goals" />
+            </p>
+            <div className="mb-3">
+              <p className="d-sm-inline-block mb-1">
+                {product.goals.map(i => {
+                  return (
+                    <Badge color="outline-info mb-1 mr-1" pill>
+                      {i}
+                    </Badge>
+                  )
+                })}
+              </p>
+            </div>
+
+            <p className="text-muted text-small mb-2">
+              <IntlMessages id="How" />
+            </p>
+            <div className="mb-3">
+              <p className="d-sm-inline-block mb-1">
+                {product.how.map(i => {
+                  return (
+                    <Badge color="outline-info mb-1 mr-1" pill>
+                      {i}
+                    </Badge>
+                  )
+                })}
+              </p>
+            </div>
+            <p className="text-muted text-small mb-2">
+              <IntlMessages id="Match" />
+            </p>
+            <div className="mb-3">
+              <Rating total={5} rating={5} interactive={false} />
+            </div>
+
+            <p className="text-muted text-small mb-2">
+              <IntlMessages id="Add to Favorites" />
+            </p>
+            <Button
+              outline
+              color="secondary"
+              className="mb-2"
+              onClick={this.checkButton}
+              active={this.state.isButtonActive}
+            >
+              <i className="simple-icon-heart" />
+
+            </Button>
+          </ Collapse>
+
+            <Button
+              outline
+              color={"theme-3"}
+              className={`icon-button ml-1 rotate-icon-click ${
+                this.state.collapse ? "rotate" : ""
+                }`}
+              onClick={this.toggleCard}
+            >
+              {this.state.collapse ? <i className="simple-icon-arrow-up" /> : <i className="simple-icon-arrow-down" />}
+            </Button>
         </CardBody>
       </Card>
     );

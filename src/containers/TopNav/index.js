c465b5dd086fdcca8417de843b1075b256330222
sharonKerz <sharonkerz@gmail.com>
2018-11-06 21:48:15 +0200

    removed grid on top bar

--- src/containers/TopNav/index.js
+++ src/containers/TopNav/index.js
@@ -195,57 +195,6 @@ class TopNav extends Component {
 
         <div className="ml-auto">
           <div className="header-icons d-inline-block align-middle">
-            <div className="position-relative d-none d-sm-inline-block">
-              <UncontrolledDropdown className="dropdown-menu-right">
-                <DropdownToggle className="header-icon" color="empty">
-                  <i className="simple-icon-grid" />
-                </DropdownToggle>
-                <DropdownMenu
-                  className="position-absolute mt-3"
-                  right
-                  id="iconMenuDropdown"
-                >
-                  <NavLink
-                    to="/app/dashboards/default"
-                    className="icon-menu-item"
-                  >
-                    <i className="iconsmind-Shop-4 d-block" />{" "}
-                    <IntlMessages id="menu.dashboards" />
-                  </NavLink>
-
-                  <NavLink to="/app/ui" className="icon-menu-item">
-                    <i className="iconsmind-Pantone d-block" />{" "}
-                    <IntlMessages id="menu.ui" />
-                  </NavLink>
-                  <NavLink to="/app/ui/charts" className="icon-menu-item">
-                    <i className="iconsmind-Bar-Chart d-block" />{" "}
-                    <IntlMessages id="menu.charts" />
-                  </NavLink>
-                  <NavLink
-                    to="/app/applications/chat"
-                    className="icon-menu-item"
-                  >
-                    <i className="iconsmind-Speach-BubbleDialog d-block" />{" "}
-                    <IntlMessages id="menu.chat" />
-                  </NavLink>
-                  <NavLink
-                    to="/app/applications/survey"
-                    className="icon-menu-item"
-                  >
-                    <i className="iconsmind-Formula d-block" />{" "}
-                    <IntlMessages id="menu.survey" />
-                  </NavLink>
-                  <NavLink
-                    to="/app/applications/todo"
-                    className="icon-menu-item"
-                  >
-                    <i className="iconsmind-Check d-block" />{" "}
-                    <IntlMessages id="menu.todo" />
-                  </NavLink>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </div>
-
             <div className="position-relative d-inline-block">
               <UncontrolledDropdown className="dropdown-menu-right">
                 <DropdownToggle
